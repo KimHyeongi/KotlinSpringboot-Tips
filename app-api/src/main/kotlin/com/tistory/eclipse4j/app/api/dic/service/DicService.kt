c65bb0416a8ffe29c69f5ed7d4e42ee8725a791f
김현기 <grissom@woowahan.com>
2021-10-24 09:28:56 +0900

    runCatching

--- app-api/src/main/kotlin/com/tistory/eclipse4j/app/api/dic/service/DicService.kt
+++ app-api/src/main/kotlin/com/tistory/eclipse4j/app/api/dic/service/DicService.kt
@@ -1,7 +1,9 @@
 package com.tistory.eclipse4j.app.api.dic.service
 
 import com.tistory.eclipse4j.app.api.dic.data.Dic
+import com.tistory.eclipse4j.domain.persist.dic.entity.DicEntity
 import com.tistory.eclipse4j.domain.persist.dic.service.DicFindService
+import mu.KotlinLogging
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
@@ -10,11 +12,13 @@ import org.springframework.stereotype.Service
 @Service
 class DicService(val dicService: DicFindService) {
 
+    val log = KotlinLogging.logger {  }
     @Cacheable(value = ["cached_dic_by_id"], key = "#id")
-    fun findById(id: Long): Dic {
-        val dicEntity = dicService.findById(id)
-        return dicEntity.let { Dic.from(it) }
-    }
+    fun findById(id: Long): Dic? =
+        kotlin.runCatching { dicService.findById(id) }
+            .map { Dic.from(it) }
+            .getOrThrow()
+
 
     fun findAllLimit(limit: Int): Page<Dic> {
         val page = dicService.findAllPageable(PageRequest.of(0, limit))
