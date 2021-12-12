a34e0d1e8871f8e349108ac7194b65e208400540
김현기 <grissom@woowahan.com>
2021-12-12 12:14:46 +0900

    Webclient Executors.newFixedThreadPool

--- domain-service/src/main/kotlin/com/tistory/eclipse4j/domain/persist/api/userinfo/service/UserInfoClientService.kt
+++ domain-service/src/main/kotlin/com/tistory/eclipse4j/domain/persist/api/userinfo/service/UserInfoClientService.kt
@@ -3,6 +3,7 @@ package com.tistory.eclipse4j.domain.persist.api.userinfo.service
 import com.tistory.eclipse4j.domain.persist.api.userinfo.body.UserInfoBody
 import com.tistory.eclipse4j.domain.persist.api.userinfo.body.UserInfoResponse
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.springframework.beans.factory.annotation.Qualifier
@@ -10,6 +11,7 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
+import java.util.concurrent.Executors
 
 @Service
 class UserInfoClientService(
@@ -19,12 +21,23 @@ class UserInfoClientService(
 
     fun getRunBlockingUserInfo(userId: String): UserInfoResponse<UserInfoBody> {
         return runBlocking<UserInfoResponse<UserInfoBody>> {
-            getUserInfoByUserId(userId)
+            getUserInfoByUserId_DispatchersIO(userId)
         }
     }
 
-    private suspend fun getUserInfoByUserId(userId: String): UserInfoResponse<UserInfoBody> {
+    // https://kt.academy/article/cc-dispatchers
+    private suspend fun getUserInfoByUserId_DispatchersIO(userId: String): UserInfoResponse<UserInfoBody> {
         return withContext(Dispatchers.IO) {
+            userInfoWebClient.get().uri("/users/{userId}/info", userId)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .awaitBody<UserInfoResponse<UserInfoBody>>()
+        }
+    }
+
+    private suspend fun getUserInfoByUserId_Executors(userId: String): UserInfoResponse<UserInfoBody> {
+        val executor = Executors.newFixedThreadPool(3)
+        return withContext(executor.asCoroutineDispatcher()) {
             userInfoWebClient.get().uri("/users/{userId}/info", userId)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
